6e1bbfb258a55079d7f1099495dccd6db679f52f
lprodrigues451 <lprodrigues451@gmail.com>
2022-05-25 18:18:30 -0300

    Subindo o exercicio2

---
+++ Exercicio2/app/src/main/java/br/com/zup/meupet/MainActivity.kt
@@ -0,0 +1,20 @@
+package br.com.zup.meupet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val botaoMensagem = findViewById<Button>(R.id.botao_mensagem)
+
+        botaoMensagem.setOnClickListener{
+            Toast.makeText(this, "Oi meu nome é Branquinho", Toast.LENGTH_LONG).show()
+        }
+
+    }
+}
